c91c472e69c98736f627c067058b67cd120d2798
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-03-14 18:34:05 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
-
-<?php //google
+<?php
 
 //config.php
 
@@ -10,13 +9,13 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('947482041440-33e8qh9nlu3vemrq1brcp9jdbb1a582v.apps.googleusercontent.com');
+$google_client->setClientId('539246690236-tvoad6op2k24t4010o2re4nd9v2kjq1n.apps.googleusercontent.com');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('ACl_dvm1FbABsfeSpnR9QxvX');
+$google_client->setClientSecret('WpZooC_GLJZXb9zunI3SH828');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('http://localhost/api/index.php');
+$google_client->setRedirectUri('https://maja-etact.herokuapp.com/index.php');
 
 //
 $google_client->addScope('email');
@@ -25,6 +24,24 @@ $google_client->addScope('profile');
 
 //start session on web page
 session_start();
-
+// credits: john paul majaaaaaa
 ?>
 
+<?php //facebook
+
+require_once 'vendor/autoload.php';
+
+if (!session_id())
+{
+    session_start();
+}
+
+// Call Facebook API
+
+$facebook = new \Facebook\Facebook([
+  'app_id'      => '2621185531323878',
+  'app_secret'     => 'e96b253c6d17a45a674710eefe2153f1',
+  'default_graph_version'  => 'v2.10'
+]);
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET['navigation']: '';
-
+//credits: john paul maja :D <3
 include('config.php');
 
 $login_button = '';
@@ -35,12 +35,66 @@ if(!isset($_SESSION['access_token'])){
 
 ?>
 
+<?php
+include('config.php');
+$facebook_output = '';
+$facebook_helper = $facebook->getRedirectLoginHelper();
 
+if(isset($_GET['code']))
+{
+ if(isset($_SESSION['access_token']))
+ {
+  $access_token = $_SESSION['access_token'];
+ }
+ else
+ {
+  $access_token = $facebook_helper->getAccessToken();
 
+  $_SESSION['access_token'] = $access_token;
+
+  $facebook->setDefaultAccessToken($_SESSION['access_token']);
+ }
+ $_SESSION['user_id'] = '';
+ $_SESSION['user_name'] = '';
+ $_SESSION['user_email_address'] = '';
+ $_SESSION['user_image'] = '';
+
+ $graph_response = $facebook->get("/me?fields=name,email", $access_token);
+
+ $facebook_user_info = $graph_response->getGraphUser();
+
+ if(!empty($facebook_user_info['id']))
+ {
+  $_SESSION['user_image'] = 'http://graph.facebook.com/'.$facebook_user_info['id'].'/picture';
+ }
+
+ if(!empty($facebook_user_info['name']))
+ {
+  $_SESSION['user_name'] = $facebook_user_info['name'];
+ }
+
+ if(!empty($facebook_user_info['email']))
+ {
+  $_SESSION['user_email_address'] = $facebook_user_info['email'];
+ }
+ 
+}
+else
+{
+ // Get login url
+    $facebook_permissions = ['email']; // Optional permissions
+
+    $facebook_login_url = $facebook_helper->getLoginUrl('https://maja-etact.herokuapp.com/', $facebook_permissions);
+    
+    // Render Facebook login button
+    $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="php-login-with-facebook.gif" /></a></div>';
+}
+
+?>
 
 <html> 
-    <head>
-      <title>Endterm Activity #1</title>
+    <head> <!-- credits: john paul maja :D <3 -->
+      <title>Endterm Exam</title>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
       <link rel="stylesheet" type="text/css" href="css/style.css">
       <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Fira+Sans|Mukta&display=swap" rel="stylesheet">
@@ -49,7 +103,7 @@ if(!isset($_SESSION['access_token'])){
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
       
     </head>
-    <body>
+    <body> <!-- credits: john paul maja :D <3 -->
       <div class="header">
       <a href="index.php"><img src="images/logo.png" style="width:300px;height:200px;"></a>
       </div>
@@ -59,12 +113,52 @@ if(!isset($_SESSION['access_token'])){
         <a href="index.php?navigation=product">Products</a>
         <a href="index.php?navigation=categories">Category</a>
         <a href="index.php?navigation=create">Create</a>
+        <a href="index.php?navigation=technology">Technology News</a>
+        <a href="index.php?navigation=business">Business News</a>
       </div>
 
         <div class="panel panel-default">
-        <?php
+        <?php //<!-- credits: john paul maja :D <3 -->
         if($login_button == '')
         {
+          switch($navigation){
+            case 'product':
+              require_once 'product.php';
+              break;
+            case 'categories':
+              require_once 'categories.php';
+              break;
+            case 'create':
+              require_once 'form_create.php';
+              break;
+            case 'details':
+              require_once 'product-details.php';
+              break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+            case 'technology':
+              require_once 'technology.php';
+              break;
+            case 'business':
+              require_once 'business.php';
+              break;
+            default:
+              require_once 'home.php';
+              break;
+          }
+        }
+        else
+        {
+          echo '<div align="center">'.$login_button . '</div>';
+        }
+        ?>
+
+        <?php 
+        if(isset($facebook_login_url)){
+            echo $facebook_login_url;
+          }
+        else{
           switch($navigation){
             case 'product':
               require_once 'product.php';
@@ -84,19 +178,14 @@ if(!isset($_SESSION['access_token'])){
             default:
               require_once 'home.php';
               break;
+            }
           }
-        }
-        else
-        {
-          echo '<div align="center">'.$login_button . '</div>';
-        }
         ?>
-
         </div>
       <div class="footer">
-        <h1>Maja | API</h2>
+        <h1>Maja | API</h2> <!-- credits: john paul maja :D <3 -->
       </div>
-    </body>
+    </body><!-- credits: john paul maja :D <3 -->
 </html>
 
 
