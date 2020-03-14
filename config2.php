bd3f11f8ab5e7aa4ca5813425dc9ebf7af49aa60
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-03-14 19:23:04 +0800

    Update config2.php

--- config2.php
+++ config2.php
@@ -10,8 +10,8 @@ if (!session_id())
 // Call Facebook API
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '568286157120937',
-  'app_secret'     => '1e3a40ac75b79a33fbf48f70731d4616',
+  'app_id'      => '2621185531323878',
+  'app_secret'     => 'e96b253c6d17a45a674710eefe2153f1',
   'default_graph_version'  => 'v2.10'
 ]);
 
