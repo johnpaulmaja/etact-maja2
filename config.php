98c5ecf3900477da2cea158fa339cdb1a4b4ed63
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-03-14 20:43:37 +0800

    Update config.php

--- config.php
+++ config.php
@@ -23,13 +23,6 @@ $google_client->addScope('email');
 $google_client->addScope('profile');
 
 
-$facebook = new \Facebook\Facebook([
-    'app_id'      => '2621185531323878',
-    'app_secret'     => 'e96b253c6d17a45a674710eefe2153f1',
-    'default_graph_version'  => 'v2.10'
-  ]);
-
-
 //start session on web page
 session_start()
 // credits: john paul majaaaaaa
