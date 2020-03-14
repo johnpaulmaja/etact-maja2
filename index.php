e7111f03cf49c3b71eb1dacbf618bc6f8fd5bdb5
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-03-14 18:59:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -32,64 +32,6 @@ if(isset($_GET["code"])){
 if(!isset($_SESSION['access_token'])){
  $login_button = '<br><a href="'.$google_client->createAuthUrl().'"><img style="" src="images/google.png" /></a>';
 }
-
-?>
-
-<?php
-include('config.php');
-$facebook_output = '';
-$facebook_helper = $facebook->getRedirectLoginHelper();
-
-if(isset($_GET['code']))
-{
- if(isset($_SESSION['access_token']))
- {
-  $access_token = $_SESSION['access_token'];
- }
- else
- {
-  $access_token = $facebook_helper->getAccessToken();
-
-  $_SESSION['access_token'] = $access_token;
-
-  $facebook->setDefaultAccessToken($_SESSION['access_token']);
- }
- $_SESSION['user_id'] = '';
- $_SESSION['user_name'] = '';
- $_SESSION['user_email_address'] = '';
- $_SESSION['user_image'] = '';
-
- $graph_response = $facebook->get("/me?fields=name,email", $access_token);
-
- $facebook_user_info = $graph_response->getGraphUser();
-
- if(!empty($facebook_user_info['id']))
- {
-  $_SESSION['user_image'] = 'http://graph.facebook.com/'.$facebook_user_info['id'].'/picture';
- }
-
- if(!empty($facebook_user_info['name']))
- {
-  $_SESSION['user_name'] = $facebook_user_info['name'];
- }
-
- if(!empty($facebook_user_info['email']))
- {
-  $_SESSION['user_email_address'] = $facebook_user_info['email'];
- }
- 
-}
-else
-{
- // Get login url
-    $facebook_permissions = ['email']; // Optional permissions
-
-    $facebook_login_url = $facebook_helper->getLoginUrl('https://maja-etact.herokuapp.com/', $facebook_permissions);
-    
-    // Render Facebook login button
-    $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="php-login-with-facebook.gif" /></a></div>';
-}
-
 ?>
 
 <html> 
@@ -153,34 +95,7 @@ else
           echo '<div align="center">'.$login_button . '</div>';
         }
         ?>
-
-        <?php 
-        if(isset($facebook_login_url)){
-            echo $facebook_login_url;
-          }
-        else{
-          switch($navigation){
-            case 'product':
-              require_once 'product.php';
-              break;
-            case 'categories':
-              require_once 'categories.php';
-              break;
-            case 'create':
-              require_once 'form_create.php';
-              break;
-            case 'details':
-              require_once 'product-details.php';
-              break;
-            case 'update':
-              require_once 'form_update.php';
-              break;
-            default:
-              require_once 'home.php';
-              break;
-            }
-          }
-        ?>
+         
         </div>
       <div class="footer">
         <h1>Maja | API</h2> <!-- credits: john paul maja :D <3 -->
