faeac1060c43bb92bb9fa6d751eab7d5a3fffb24
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-03-16 11:58:20 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <h1>Welcome User</h1>
 
 
-<!--google-->
+<!--google credits by m-a-j-a-->
 <div style="background-color: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   margin-bottom: 25px;
@@ -13,9 +13,9 @@
   padding: 10px 20px;">
   <h1>Profile Picture</h1><!-- credits: john paul maja :D <3 -->
   </div><!-- credits: john paul maja :D <3 -->
-</div>
+</div><!--credits by m-a-j-a-->
 <h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1>
 <h1>Email: <?php echo $_SESSION['user_email_address'] ?></h1>
 
-<h1><a href="logout-google.php">Logout</a></h1>
+<h1><a href="logout-google.php">Logout</a></h1><!--credits by m-a-j-a-->
 
