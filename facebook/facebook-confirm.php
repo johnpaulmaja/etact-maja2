79ed215aaea7bf9f9b45c0aab478a4747913fff9
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-03-18 19:18:30 +0800

    Update facebook-confirm.php

--- facebook/facebook-confirm.php
+++ facebook/facebook-confirm.php
@@ -38,6 +38,7 @@ if(isset($_GET['code'])){
  if(!empty($facebook_user_info['email'])){
   $_SESSION['user_email_address'] = $facebook_user_info['email'];
  }
+ header('location:index.php');
 }else{
  // Get login url  credits: john paul maja :D <3 
     $facebook_permissions = ['email']; // Optional permissions  credits: john paul maja :D <3 
